8e1bfe9e23d849350b8eae22a51e0e70d8f2ce6b
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-05-16 03:37:30 +0000

    yop

--- main.cs
+++ main.cs
@@ -1,4 +1,4 @@
-// Created by:Emmanuel Fofeyin
+// Created by: Zaida Hammel
 // Created on: May 2022
 //
 // This program accepts user input
@@ -9,30 +9,28 @@ class Program
 {
     public static void Main(string[] args)
     {
-        int lengtha;
-        int lengthb;
-        int lengthc;
+        int side1;
+        int side2;
+        int side3;
 
         Console.Write("Enter the lengths of the sides of your triangle.");
         Console.WriteLine("");
-        Console.Write("Enter length 1: ");
-        lengtha = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter side 1: ");
+        side1 = Convert.ToInt32(Console.ReadLine());
         Console.Write("");
-        Console.Write("Enter length 2: ");
-        lengthb = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter side 2: ");
+        side2 = Convert.ToInt32(Console.ReadLine());
         Console.Write("");
-        Console.Write("Enter length 3: ");
-        lengthc = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter side 3: ");
+        side3 = Convert.ToInt32(Console.ReadLine());
 
-        if (lengtha > 0 && lengthb > 0 && lengthc > 0)
-            if (lengtha == lengthb && lengthb == lengthc)
-                Console.Write("It is an equilateral Triangle.");
-            else if (lengtha == lengthb || lengthb == lengthc || lengtha == lengthc)
-                Console.Write("It is an isosceles Triangle.");
-            else if (lengtha != lengthb && lengthb != lengthc && lengthc != lengtha)
-                Console.Write("It is a scalene Triangle.");
-              else
-            Console.Write("You cannot input negative numbers!");
-      Console.WriteLine("\nDone.");
+        if (side1 > 0 && side2 > 0 && side3 > 0)
+            if (side1 == side2 && side2 == side3)
+                Console.Write("Your triangle is an equilateral triangle.");
+            else if (side1 == side2 || side2 == side3 || side1 == side3)
+                Console.Write("Your triangle is an isosceles triangle.");
+            else
+                Console.Write("Your triangle is an isosceles triangle.");
+            Console.WriteLine("\nDone.");
     }
 }
