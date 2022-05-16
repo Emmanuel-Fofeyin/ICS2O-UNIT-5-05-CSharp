29259f5ca4914e22fe49b46136cdcf7d586c3459
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-05-16 02:15:16 +0000

    minimalized problems

--- main.cs
+++ main.cs
@@ -3,56 +3,47 @@
 //
 // This program accepts user input
 
-
 using System;
 
-class Program
-{
-    public static void Main(string[] args)
-    {
-        // This function accepts user input
-        int lengtha;
-        int lengthb;
-        int lengthc;
+class Program {
+  public static void Main(string[] args) {
+    // This function accepts user input
+    int lengtha;
+    int lengthb;
+    int lengthc;
 
-        Console.WriteLine("Enter the Lengths of the sides on the triangle");
-        Console.WriteLine("to figure out what type of triangle it is...");
-        Console.WriteLine("");
+    Console.WriteLine("Enter the Lengths of the sides on the triangle");
+    Console.WriteLine("to figure out what type of triangle it is...");
+    Console.WriteLine("");
 
-        Console.Write("Length 1: ");
-        lengtha = Convert.ToInt32(Console.ReadLine());
-        Console.Write("");
-        Console.Write("Length 2: ");
-        lengthb = Convert.ToInt32(Console.ReadLine());
-        Console.Write("");
-        Console.Write("Length 3: ");
-        lengthc = Convert.ToInt32(Console.ReadLine());
-        Console.Write("");
+    Console.Write("Length 1: ");
+    lengtha = Convert.ToInt32(Console.ReadLine());
+    Console.Write("");
+    Console.Write("Length 2: ");
+    lengthb = Convert.ToInt32(Console.ReadLine());
+    Console.Write("");
+    Console.Write("Length 3: ");
+    lengthc = Convert.ToInt32(Console.ReadLine());
+    Console.Write("");
 
-        if (lengtha <= 0 || lengthb <= 0 || lengthc <= 0 )
-        {
-            Console.Write("Something went wrong...");
-        }
-        else
-        {
-        if (lengtha == lengthb && lengthb == lengthc && lengthc == lengtha)
-        {
-            Console.Write(" It is an equilateral Triangle.");
-        }
-        else if (lengtha == lengthb || lengthb == lengthc || lengthc == lengtha)
-        {
-            Console.Write("It is an isosceles Triangle.");
-        }
-        else if (lengtha != lengthb && lengthb != lengthc && lengthc != lengtha)
-        {
-            Console.Write("It is a scalene Triangle.");
-        }
-        else
-        {
-            Console.Write("Something went wrong...");
-        }
-
-        Console.WriteLine("\nDone.");
-        }
+    if (lengtha <= 0 || lengthb <= 0 || lengthc <= 0) {
+      Console.Write("Something went wrong...");
     }
+    else {
+      if (lengtha == lengthb && lengthb == lengthc && lengthc == lengtha) {
+        Console.Write(" It is an equilateral Triangle.");
+      }
+      else if (lengtha == lengthb || lengthb == lengthc || lengthc == lengtha) {
+        Console.Write("It is an isosceles Triangle.");
+      }
+      else if (lengtha != lengthb && lengthb != lengthc && lengthc != lengtha) {
+        Console.Write("It is a scalene Triangle.");
+      }
+      else {
+        Console.Write("Something went wrong...");
+      }
+
+      Console.WriteLine("\nDone.");
+    }
+  }
 }
